a23bd27ddd4eab3950144d0b9b0a5167539ac78c
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-14 13:01:42 -0300

    Agregado Controllers/LoginController.php

---
+++ controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+    public static function login() {
+        echo "Desde Login";
+    }
+
+    public static function logout() {
+        echo "Desde Logout";
+    }
+}
