da2eca69ebddf553714459b0bf5f2d43604e15f7
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-15 19:45:58 -0300

    Añadido form al login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,21 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión con tus Datos</p>
+
+<form action="/" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Tu Email">
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Tu Password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún No Tienes una Cuenta? Crear Cuenta</a>
+    <a href="/olvide">¿Olvidaste tu Password?</a>
+</div>
