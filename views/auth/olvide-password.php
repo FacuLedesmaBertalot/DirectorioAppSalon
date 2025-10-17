3870c8ac4e15646e8496aa7756ba29a2dbba2150
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-17 15:04:00 -0300

    Nuevo archivo olvide-password

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,14 @@
+<h1 class="nombre-pagina">Olvide Password</h1>
+<p class="descripcion-pagina">Restablece Tu Password Escribiendo tu E-mail a Continuación</p>
+
+
+<form action="/olvide" method="POST" class="formulario">
+
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Tu Email">
+    </div>
+
+    <input type="submit" class="boton" value="Enviar Instrucciones">
+
+</form>
