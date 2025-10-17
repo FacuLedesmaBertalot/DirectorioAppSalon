8f767f413dc60d1755b46019b14d5485cecd4fd3
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-17 13:49:58 -0300

    Creando pagina crear-cuenta.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -23,7 +23,9 @@ class LoginController {
         echo "Desde Recuperar";
     }
 
-    public static function crear() {
-        echo "Desde Crear";
+    public static function crear(Router $router) {
+        $router->render('auth/crear-cuenta', [
+
+        ]);
     }
 }
