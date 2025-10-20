24aea36d3609b057f570ba2df3a7f6ad8eea94d6
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-20 16:14:48 -0300

    Añadiendo html y clases al cita/index.php

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,3 +1,23 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
+<p class="descripcion-pagina">Elige tus Servicios y Coloca Tus Datos</p>
 
-<p class="descripcion-pagina">Elige tus Servicios a Continuación</p>
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a Continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+
+    </div>
+
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p>Coloca tus Datos y Fecha de tu Cita</p>
+
+    </div>
+
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que la Información sea Correcta</p>
+
+    </div>
+</div>
