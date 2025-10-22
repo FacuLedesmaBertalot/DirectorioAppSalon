19859abbddb9635d6898716a2e5b5b0686f37883
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-22 15:36:12 -0300

    Seleccionando servicios para la cita

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita= {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
 });
@@ -130,6 +137,9 @@ function mostrarServicios(servicios) {
         const servicioDiv = document.createElement('DIV');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function() {
+            seleccionarServicio(servicio);
+        }
 
         servicioDiv.appendChild(nombre);
         servicioDiv.appendChild(precioServicio);
@@ -137,4 +147,12 @@ function mostrarServicios(servicios) {
         document.querySelector('#servicios').appendChild(servicioDiv);
 
     });
+}
+
+function seleccionarServicio(servicio) {
+    const { servicios } = cita;
+
+    cita.servicios = [...servicios, servicio];
+
+    console.log(cita);
 }
