ab64bbaed80e268025ca102d952e1ec1f1addccc
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-22 19:33:42 -0300

    Mostrar el resumen de la cita

--- src/js/app.js
+++ src/js/app.js
@@ -26,6 +26,9 @@ function iniciarApp() {
     nombreCliente(); // Añade el nombre del cliente al objeto de cita
     seleccionarFecha(); // Añade la fecha de la cita en el objeto
     seleccionarHora(); // Añade la hora de la cita en el objeto
+
+
+    mostrarResumen(); // Muestra el resumen de la cita
 }
 
 
@@ -58,6 +61,8 @@ function tabs() {
 
     botones.forEach( boton => {
         boton.addEventListener('click', function(e) {
+            e.preventDefault();
+
             paso = parseInt(e.target.dataset.paso);
 
             mostrarSeccion();
@@ -77,6 +82,8 @@ function botonesPaginador() {
     } else if (paso === 3) {
         paginaAnterior.classList.remove('ocultar');
         paginaSiguiente.classList.add('ocultar');
+
+        mostrarResumen();
     } else {
         paginaAnterior.classList.remove('ocultar');
         paginaSiguiente.classList.remove('ocultar');
@@ -234,4 +241,15 @@ function mostrarAlerta(mensaje, tipo) {
     setTimeout(() => {
         alerta.remove();
     }, 3000);
+}
+
+
+function mostrarResumen() {
+    const resumen = document.querySelector('.contenido-resumen');
+
+    if (Object.values(cita).includes('')) {
+        console.log('Hacen falta datos');
+    } else {
+        console.log('Todo bien');
+    }
 }
