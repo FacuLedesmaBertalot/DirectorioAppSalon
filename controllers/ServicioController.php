45fd6512b9cb669aef366c3fb00fca2710d496ea
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-29 19:06:18 -0300

    Creando el Formulario de Actualizar Servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -54,13 +54,25 @@ class ServicioController {
 
 
     public static function actualizar(Router $router) {
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $id = is_numeric($_GET['id']);
+        if (!$id) return; 
+        $servicio = Servicio::find($id);
+        $alertas = [];
+        
         
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
 
         $router->render('servicios/actualizar', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
 
