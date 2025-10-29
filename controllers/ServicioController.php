7536b29d604d719baddb56e79bdb4010bbe8ac14
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-10-29 17:43:46 -0300

    Creando las vistas para los Servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -6,17 +6,31 @@ use MVC\Router;
 
 class ServicioController {
     public static function index(Router $router) {
-        
-        $router->render('servicios/index', [
 
+        // Comprueba si la sesión no está ya iniciada antes de llamarla
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $router->render('servicios/index', [
+            'nombre' => $_SESSION['nombre']
         ]);
     }
 
     public static function crear(Router $router) {
+
+        // Comprueba si la sesión no está ya iniciada antes de llamarla
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         }
+
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
 
@@ -25,6 +39,10 @@ class ServicioController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function eliminar(Router $router) {
---
+++ views/servicios/crear.php
@@ -0,0 +1,6 @@
+<h1 class="nombre-pagina">Nuevo Servicio</h1>
+<p class="descripcion-pagina">Llena Todos los Campos para Añadir un Nuevo Servicio</p>
+
+<?php 
+include_once __DIR__ . '/../templates/barra.php';
+?>
